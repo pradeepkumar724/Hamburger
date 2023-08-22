a2d36597ee24b6eaddb8a486af98a5a57186c1fa
pradeep <724pradeep@gmail.com>
2023-08-22 22:28:35 +0530

    Hamberger

---
+++ index.js
@@ -0,0 +1,5 @@
+let hamburger = document.getElementById("hamburger");
+hamburger.onclick = function () {
+  let navBar = document.getElementById("nav-bar");
+  navBar.classList.toggle("active");
+};
